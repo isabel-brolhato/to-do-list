a14b3406115744eed25f7d6253eb7b601454f69d
Isabel <isabel.brolhato@gmail.com>
2022-06-05 20:39:23 -0300

    adds form, makes list, adds and removes items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <h1>To Do List</h1>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  updateInput(key, value) {
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    const list = [...this.state.list];
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="title">To Do List</h1>
+        <div className="container">
+          <input
+            type="text"
+            placeholder="Add item name"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            className="add-btn"
+            onClick={() => this.addItem()}
+          >Add</button>
+          <br />
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button className="delete-btn" onClick={() => this.deleteItem(item.id)}>Delete</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
