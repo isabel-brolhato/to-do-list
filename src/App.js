f5fcb67d714991bb13ebdeafab54cd5fd1451a6d
isabel-brolhato <102561680+isabel-brolhato@users.noreply.github.com>
2022-06-10 20:00:47 -0300

    adds form, makes list, adds and removes items (#11)
    * Adds form, makes list, adds and removes items
    
    * Add components, disable add button when input is empty

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
-import './App.css';
+import React from 'react';
+import AddBtn from './Components/AddBtn';
+import Title from './Components/Title';
 
-function App() {
-  return (
-    <h1>To Do List</h1>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  updateInput(key, value) {
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    const list = [...this.state.list];
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div>
+        <AddBtn />
+<Title />
+        <div className="container">
+          <input
+            type="text"
+            placeholder="Add item name"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            className="add-btn"
+            onClick={() => this.addItem()}
+            disabled={!this.state.newItem.length}
+          >Add</button>
+          <br />
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button className="delete-btn" onClick={() => this.deleteItem(item.id)}>Delete</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Components/Title.js
@@ -0,0 +1,6 @@
+
+function Title (){
+    return <h1 className="title">To Do List</h1>
+}
+
+export default Title;
