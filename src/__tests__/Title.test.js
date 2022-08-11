8e7e75c19cdd487d421d018f44e87bef38c6e466
Wiktor Doberschuetz <wdoberschuetz@gmail.com>
2022-08-11 17:39:51 +0200

    Add a placeholder test for Title

--- src/__tests__/Title.test.js
+++ src/__tests__/Title.test.js
@@ -2,7 +2,13 @@ import { render, screen } from "@testing-library/react";
 import Title from "../Components/Title";
 
 test("Should contain title", () => {
-    render(<Title />);
-    
-    expect(screen.getByTestId('title')).toBeInTheDocument()
-  });
+  render(<Title />);
+
+  expect(screen.getByTestId("title")).toBeInTheDocument();
+});
+
+test("Should have a title 'To Do List'", () => {
+  render(<Title />);
+
+});
+
